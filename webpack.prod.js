398e4831c4ecb621b81a8e85f4fe8965d252f48d
cedafiso <cedafiso@gmail.com>
2021-09-06 13:34:01 -0700

    Fixing react dates css

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = merge(common, {
-    plugins:[new MiniCssExtractPlugin()],
+    plugins:[new MiniCssExtractPlugin('styles.css')],
     module: {
         rules: [{
             loader:'babel-loader',
